540c73158186014a26623be3726e5139b4244b07
yamaoka <yamaoka@example.com>
2017-10-12 11:46:27 +0900

    グラフのリンク 月の画面のリンク

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,39 @@ Rails.application.routes.draw do
   resources :plans
   resources :diaries 
   resources :hearts
+  resources :chart
+  resources :january
+  resources :february
+  resources :march
+  resources :april
+  resources :may
+  resources :june
+  resources :july
+  resources :august
+  resources :september
+  resources :october
+  resources :november
+  resources :december
   get 'home/calendar'
   get 'home/main'
-  get 'home/calendar'
+  #get 'home/calendar'
 
-  resources :hearts
-  get 'home/main'
-  get 'home/menu'  
+  #resources :hearts
+  #get 'home/main'
+  get 'home/menu' 
+  get 'home/chart' 
+  get 'home/january' 
+  get 'home/february' 
+  get 'home/march' 
+  get 'home/april' 
+  get 'home/may' 
+  get 'home/june' 
+  get 'home/july' 
+  get 'home/august' 
+  get 'home/september' 
+  get 'home/october' 
+  get 'home/november' 
+  get 'home/december' 
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
