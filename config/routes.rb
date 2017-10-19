d53eb2390e1606930ad5733901b52fe68f2b267e
ooguni <ooguni@example.com>
2017-10-19 09:41:24 +0900

    カレンダーの日付にリンクをつけた

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,7 @@ Rails.application.routes.draw do
   get 'home/calendar'
   get 'home/main'
   get 'home/calendar'
-
-  resources :hearts
-  get 'home/main'
   get 'home/menu'  
+  get 'home/select'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
