ed2c49bd66022ecabea753467ff18199aef9cbd3
yamaoka <yamaoka@example.com>
2017-11-01 09:50:07 +0900

    あ

--- db/migrate/20171019012124_change_colmn_hearts.rb
+++ db/migrate/20171019012124_change_colmn_hearts.rb
@@ -1,5 +1,13 @@
 class ChangeColmnHearts < ActiveRecord::Migration[5.1]
-  def change
-    change_column :hearts, :feeling, :integer    
+#  def change
+#    change_column :hearts, :feeling, :integer    
+#  end
+
+  def up
+    change_column :hearts, :feeling, :integer
+  end
+
+  def down
+    change_column :hearts, :feeling, :string
   end
 end
